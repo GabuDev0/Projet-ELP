8071bbc1d114f5e70b5febffcea90f7168d8e452
GabuWaSaikou <contact.saikou.games@gmail.com>
2025-12-22 18:41:28 -0300

    FEAT: debug.go for plotting samples

---
+++ debug.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+)
+
+func plotSamplesFromFloats(samples []float64, filename string) error {
+	p := plot.New()
+	p.Title.Text = "Samples audio"
+	p.X.Label.Text = "Index"
+	p.Y.Label.Text = "Amplitude"
+
+	pts := make(plotter.XYs, len(samples))
+	for i, s := range samples {
+		pts[i].X = float64(i)
+		pts[i].Y = s
+	}
+
+	line, err := plotter.NewLine(pts)
+	if err != nil {
+		return err
+	}
+
+	p.Add(line)
+	
+	save_err := p.Save(12*vg.Inch, 4*vg.Inch, filename)
+
+	if save_err != nil {
+		print("Save error when creating plot")
+		
+	} else
+	{
+		fmt.Println("Plot saved at " + filename)
+	}
+
+	return save_err
+}
