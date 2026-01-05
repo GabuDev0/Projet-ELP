cecb61b7df668a7555789265d9b7eb97119da936
GabuWaSaikou <contact.saikou.games@gmail.com>
2026-01-05 17:26:55 +0100

    FIX: comments + better code writing

--- intercorrelation.go
+++ intercorrelation.go
@@ -1,24 +1,29 @@
 package main
 
 func intercorrelation(x, y []float64) []float64 {
-	
 	n := len(x)
 	m := len(y)
 
-	r := make([]float64, n+m-1)
-	k := 0
+	result := make([]float64, n+m-1)
+	k := 0 // index of result for a certain lag (ranges from 0 to n+m-1)
 
+	// Corresponds to each offset from the first non-zero position
+	// (first element of x with last element of y)
+	// to the last non-zero position
+	// (last element of x with first element of y) 
 	for lag := -m+1; lag < n; lag++ {
 		var sum float64 = 0
 		for i := 0; i < min(n, m); i++ {
-			// If in the x list
-			if i+lag >= 0 && i+lag < n {
-				sum += x[i+lag] * y[i]
+			// If element index is outside the x list, product = 0
+			if i+lag < 0 || i+lag >= n {
+				continue
 			}
+
+			sum += x[i+lag] * y[i]
 		}
-		r[k] = sum
+		result[k] = sum
 		k++
 	}
 
-	return r
+	return result
 }
