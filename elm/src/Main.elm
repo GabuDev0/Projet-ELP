7d7f0784c6a66ab466910b03d542f4c75ebe669a
GabuWaSaikou <contact.saikou.games@gmail.com>
2026-01-16 11:42:38 +0100

    elm: FEAT: Buttons update number input

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (Model)
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Browser
+import Html.Events exposing (onInput)
+import Html.Attributes exposing (placeholder)
 
 
 type alias Model =
@@ -11,15 +13,27 @@ initialModel : Int
 initialModel =
     0
 
+
+setModel : Maybe Int -> Int -> Model
+setModel maybeNum model =
+    case maybeNum of
+        Just num -> num
+        Nothing -> model
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
+        [ text "click on those buttons"
+        , button [ onClick Decrement ] [ text "-" ]
         , text (String.fromInt model)
         , button [ onClick Increment] [ text "+" ]
+        , textarea [ placeholder "Write some numbers here", onInput TextInput] [ ]
         ]
 
-type Msg = Decrement | Increment
+type Msg = Decrement | Increment | TextInput String
+
+
 main : Program () Model Msg
 main =
     Browser.sandbox
@@ -29,10 +43,18 @@ main =
         }
 
 
+on_Input : msg -> msg
+on_Input str =
+    str
 
+
+
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
             model + 1
         Decrement ->
             model - 1
+        TextInput text ->
+            setModel (String.toInt text) model
