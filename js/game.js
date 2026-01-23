d8baf2264b12ef5bf72f66cc2465a66cf97cfe5d
GabuWaSaikou <contact.saikou.games@gmail.com>
2026-01-23 15:58:18 +0100

    js: FEAT: add all card types in the gameDeck

--- js/game.js
+++ js/game.js
@@ -1,13 +1,43 @@
 import NumberCard from "./numberCard.js"
+import ActionCard from "./actionCard.js"
+import ModifierCard from "./modifierCard.js";
 function fillGameDeck() {
     const gameDeck = []
+
+    // Add number cards
     for (let index = 0; index < 13; index++) {
-        const numberCard = new NumberCard("numberCard", index);
+        const numberCard = new NumberCard(index);
+        if (index == 0) {
+            gameDeck.push(numberCard);
+        }
         for (let j = 0; j < index; j++) {
             gameDeck.push(numberCard);
         }
-        
     }
+
+    // Add modifier cards
+    for (let i = 1; i < 6; i++) {
+        const modifierCardPlus = new ModifierCard("+", i*2)
+        gameDeck.push(modifierCardPlus);
+    }
+    const modifierCardMult = new ModifierCard("x", 2)
+    gameDeck.push(modifierCardMult)
+
+    
+    // Add action cards
+    for (let i = 0; i < 3; i++) {
+        const actionCard1 = new ActionCard("freeze");
+        const actionCard2 = new ActionCard("flip three");
+        const actionCard3 = new ActionCard("second chance");
+
+        gameDeck.push(actionCard1);
+        gameDeck.push(actionCard2);
+        gameDeck.push(actionCard3);
+    }
+    
+
+
+
     return gameDeck;
 }
 
