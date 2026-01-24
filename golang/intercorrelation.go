dae28296ad9a719b099f6df6527919bf626f2a05
GabuWaSaikou <contact.saikou.games@gmail.com>
2026-01-24 21:06:13 +0100

    golang: FEAT: func that verifies if a result is what we expect

--- golang/intercorrelation.go
+++ golang/intercorrelation.go
@@ -31,6 +31,22 @@ func intercorrelation(x, y []float64) []float64 {
 	return result
 }
 
+// If intercorr(x, y) == toVerify, returns False else True
+func verifyIntercorrelation(toVerify, x, y []float64) bool {
+	intercorr := intercorrelation(x, y)
+	if len(toVerify) != len(intercorr) {
+		fmt.Println("WARNING: verifyIntercorrelation(): the len of toVerify isn't the one expected.")
+		return true
+	}
+	for i := 0; i < len(toVerify); i++ {
+		if toVerify[i] != intercorr[i] {
+			return true
+		}
+	}
+
+	return false
+}
+	
 
 func testIntercorrelation() {
 	x1 := []float64{1, 1, 1, 1}
