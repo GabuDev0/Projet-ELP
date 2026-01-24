151feb7c1aadd0e9fa488ceace4b06393c6fb856
Nanyi <18167108330@163.com>
2026-01-24 22:45:37 +0100

    FEAT: Fonctions gameRound and playerTurn are now available.

--- js/game.js
+++ js/game.js
@@ -49,18 +49,22 @@ function showDeck(deck) {
 }
 
 function drawCard(deck, player){
-	const card = deck.pop();
-	if (card.type === "NumberCard") {
-		const duplicate = player.hand.some(c => c.value === card.value);
-		console.log(player.ID, "gets", card.toString());
-		if (duplicate) {
-			player.busted = true;
-			return {status: "busted", card: card};
+	if (player.hand.length < 7) {
+		const card = deck.pop();
+		if (card.type === "NumberCard") {
+			const duplicate = player.hand.some(c => c.value === card.value);
+			console.log(player.ID, "gets", card.toString());
+			if (duplicate) {
+				player.busted = true;
+				return {status: "busted", card: card};
+			}
 		}
+		
+		player.hand.push(card);
+		return {status: "ok", card: card};
+	} else {
+		return {status: "success", card: null};
 	}
-	
-	player.hand.push(card);
-	return {status: "ok", card: card};
 }
 
 function shuffleDeck(deck) {
@@ -69,6 +73,89 @@ function shuffleDeck(deck) {
 		[deck[i], deck[j]] = [deck[j], deck[i]];
 	}
 }
+
+function resetPlayerForNewTurn(player) {
+  	player.hand = [];
+  	player.busted = false;
+}
+
+function isTurnFinished(player, choice) {
+  if (choice === "stop") return true;
+  if (player.busted) return true;
+  if (player.hand.length === 7) return true;
+  return false;
+}
+
+function playerTurn (player, deck, rl, onEnd){
+	rl.question("Continue to flip? (y/n)", (answer) => {
+        if (answer === "n") {
+			console.log("Player chooses to stop. Turn ended.");
+            console.log("Hand cards:", player.hand);
+            onEnd();
+            return;
+        }
+	
+		const result = drawCard(deck, player);
+		switch (result.status) {
+			case "busted":
+				console.log("Player ", player.ID, " gets ", result.card.toString(), " and is busted. Turn ended.");
+				onEnd();
+        		return;
+			case "success":
+				console.log("Player ",player.ID, " has flipped 7 number cards successfully. Turn ended.");
+				onEnd();
+        		return;
+			case "ok":
+				console.log("Player ", player.ID, " gets ", result.card.toString(), " and issafe. Turn continues.");
+				playerTurn(player, deck, rl, onEnd);
+        		return;
+		}
+	});
+}
+
+
+function gameRound(players, deck, rl, onRoundEnd) {
+	let playerIndex = 0;
+
+	function nextPlayer() {
+		if (playerIndex >= players.length) {
+			console.log("All players have finished their turns.");
+			onRoundEnd();
+			return;
+		}
+
+		const player = players[playerIndex];
+		console.log("Player ", player.ID, "'s turn ---");
+		resetPlayerForNewTurn(player);
+		playerTurn(player, deck, rl, () => {
+			playerIndex++;
+			nextPlayer();
+		})
+	}
+
+	nextPlayer();
+}
+
+function startGame(players, deck, rl) {
+	let roundIndex = 1;
+
+	function nextRound() {
+		if (players.some(p => p.points >= 200)){
+			console.log("Game finished");
+			rl.close();
+			return;
+		}
+
+		console.log(`Round ${roundIndex}`);
+		gameRound(players, deck, rl, () => {
+			roundIndex ++;
+			nextRound();
+		});
+	}
+
+	nextRound();
+}
+
 
 // gameDeck: [Card], discardPile: [Card], playerPoints: [int]
 const saveTurnToFile = async (filename, gameDeck, discardPile, playerPoints) => {
@@ -79,6 +166,7 @@ const saveTurnToFile = async (filename, gameDeck, discardPile, playerPoints) => 
 }
 
 const gameDeck = fillGameDeck()
+shuffleDeck(gameDeck)
 showDeck(gameDeck)
 
 saveTurnToFile("data.txt", gameDeck, [], [123, 44]).then(filename => {
