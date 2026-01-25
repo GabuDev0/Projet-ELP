dfdc3a0b2dd6e0d3469c7cfa72169748eab94b3e
Nanyi <18167108330@163.com>
2026-01-25 19:27:57 +0100

    Intercorrelation with parallel go routines is now a fonction. Transformation from file to sample is now a function. Function main is therefore simplified. New function corrEnergy is created, so in main() the energy of each pitch class is calculated and stored into a map 'pcEnergy'.

--- golang/main.go
+++ golang/main.go
@@ -24,10 +24,10 @@ type Result struct {
 }
 
 // Worker
-func worker(jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup) {
+func worker(jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup, note int) {
 	defer wg.Done()
 
-	noteSamplesFloat := GetNoteSamples(61)
+	noteSamplesFloat := GetNoteSamples(note)
 
 	for job := range jobs {
 		intercorr := intercorrelation(job.data, noteSamplesFloat)
@@ -42,13 +42,15 @@ func worker(jobs <-chan Job, results chan<- Result, wg *sync.WaitGroup) {
 
 }
 
-func main() {
-	file_path := "example-files/scale_piano_C_maj.wav"
-	fmt.Println(file_path)
+func corrEnergy(corr []float64) float64 {
+	sum := 0.0
+	for _, v := range corr {
+		sum += v * v // ou math.abs(v) ?
+	}
+	return sum
+}
 
-	file, _ := os.Open(file_path)
-	reader := wav.NewReader(file)
-
+func parallelIntercorr(songSamplesFloat []float64, note int) []float64 {
 	const NUM_WORKERS = 8
 	const SAMPLES_PER_JOB = 2048
 
@@ -57,28 +59,9 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	defer file.Close()
-
-	var songSamplesFloat []float64
-
-	// Transform each Sample to a float and adds it to the songSamplesFloat slice
-	for {
-		// "samples" var is a packet of <=2048 samples (if enough samples, otherwise less)
-		samples, err := reader.ReadSamples()
-		if err == io.EOF {
-			break
-		}
-		for _, sample := range samples {
-			// the whole song samples
-			songSamplesFloat = append(songSamplesFloat, reader.FloatValue(sample, 0))
-
-		}
-
-	}
-
 	for i := 0; i < NUM_WORKERS; i++ {
 		wg.Add(1)
-		go worker(jobs, results, &wg)
+		go worker(jobs, results, &wg, note)
 	}
 
 	go func() {
@@ -90,11 +73,13 @@ func main() {
 		for {
 			// If every sample has been sent to a job
 			if i >= len(songSamplesFloat) {
-				jobs <- Job{
-					startingIndex: k,
-					lenX:          len(samplesFloat),
-					lenY:          GetSamplesNumber(),
-					data:          samplesFloat,
+				if len(samplesFloat) > 0 {
+					jobs <- Job{
+						startingIndex: k,
+						lenX:          len(samplesFloat),
+						lenY:          GetSamplesNumber(),
+						data:          samplesFloat,
+					}
 				}
 				break
 			}
@@ -131,7 +116,7 @@ func main() {
 
 	// Contains the results of the intercorrelation computed with goroutines
 	intercorrTotal := make([]float64, len(songSamplesFloat)+SAMPLESNUM-1)
-	
+
 	// Collector
 	for partial := range results {
 		fmt.Printf("Llen(partial): ")
@@ -148,9 +133,82 @@ func main() {
 		}
 	}
 
+	return intercorrTotal
+}
+
+func transformFileToSample(file_path string) []float64 {
+	fmt.Println(file_path)
+
+	file, _ := os.Open(file_path)
+	reader := wav.NewReader(file)
+
+	defer file.Close()
+
+	var songSamplesFloat []float64
+
+	// Transform each Sample to a float and adds it to the songSamplesFloat slice
+	for {
+		// "samples" var is a packet of <=2048 samples (if enough samples, otherwise less)
+		samples, err := reader.ReadSamples()
+		if err == io.EOF {
+			break
+		}
+		for _, sample := range samples {
+			// the whole song samples
+			songSamplesFloat = append(songSamplesFloat, reader.FloatValue(sample, 0))
+		}
+	}
+	return songSamplesFloat
+}
+
+func main() {
+	pitchClasses := map[string][]int{
+		"C":  {48, 60, 72},
+		"C#": {49, 61, 73},
+		"D":  {50, 62, 74},
+		"D#": {51, 63, 75},
+		"E":  {52, 64, 76},
+		"F":  {53, 65, 77},
+		"F#": {54, 66, 78},
+		"G":  {55, 67, 79},
+		"G#": {56, 68, 80},
+		"A":  {57, 69, 81},
+		"A#": {58, 70, 82},
+		"B":  {59, 71, 83},
+	}
+
+	file_path := "example-files/scale_piano_C_maj.wav"
+	songSamplesFloat := transformFileToSample(file_path)
+
+	//Store the energy of every pitch class in pcEnergy
+	pcEnergy := make(map[string]float64)
+
+	for pc, notes := range pitchClasses {
+		maxEnergy := 0.0
+		for _, note := range notes {
+			corr := parallelIntercorr(songSamplesFloat, note)
+			energy := corrEnergy(corr)
+			if energy >= maxEnergy {
+				maxEnergy = energy
+			}
+		}
+		pcEnergy[pc] = maxEnergy
+	}
+
+	fmt.Println("Pitch class energies:")
+	for pc, eng := range pcEnergy {
+		fmt.Println(pc, ":", eng)
+	}
+
+	//Example: energy of C#4
+	intercorrTotal := parallelIntercorr(songSamplesFloat, 61)
+
+	energy61 := corrEnergy(intercorrTotal)
+	fmt.Println("Energy of note 61:", energy61)
+
+	//Test of incorrelation in one go
 	noteSamplesFloat := GetNoteSamples(61)
 
-	// Test of incorrelation in one go
 	intercorr := intercorrelation(songSamplesFloat, noteSamplesFloat)
 
 	plotFloats(intercorr, "intercorrPlot61piano.jpg")
