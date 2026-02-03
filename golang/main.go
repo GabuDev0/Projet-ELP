0429aa9d712d58c4ea42524e3f404c637a51bf50
GabuWaSaikou <contact.saikou.games@gmail.com>
2026-02-03 14:26:45 +0100

    FEAT: MIT License

--- golang/main.go
+++ golang/main.go
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2026 INSA Lyon Telecoms Department
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package main
 
 import (
@@ -297,8 +321,9 @@ func main() {
 		"B":  {12},
 	}
 
-	//file_path := "example-files/a-tender-feeling-piano-torby-brand.mp3"
-	file_path := "example-files/scale_piano_C_maj.wav"
+	// The input file
+	file_path := "example-files/a-tender-feeling-piano-torby-brand.mp3"
+	//file_path := "example-files/scale_piano_C_maj.wav"
 
 	songSamplesFloat := transformFileToSample(file_path)
 
